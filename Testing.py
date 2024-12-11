5122b5b647f09a2a2ab956e17d08d7ae0c797283
difoster7 <95183391+difoster7@users.noreply.github.com>
2024-12-11 18:06:34 -0500

    added move_fwd and rotate test functions

--- Testing.py
+++ Testing.py
@@ -3,6 +3,8 @@ import TurboPi.HiwonderSDK.mecanum as mecanum
 import signal
 import time
 
+inches_to_mm = 25.4
+
 chassis = mecanum.MecanumChassis()
 
 def stop(signum, frame):
@@ -11,10 +13,23 @@ def stop(signum, frame):
 
 signal.signal(signal.SIGINT, stop)
 
+def move_fwd(inches):
+    mm = inches * inches_to_mm
+    total_drive_time = inches / 12
+    current_drive_time = 0
+    while current_drive_time < total_drive_time
+        chassis.set_velocity(12*inches_to_mm, 90, 0)
+        time.sleep(1)
+        current_drive_time += 1
+    chassis.set_velocity(0,0,0)
+
+def rotate():
+    chassis.set_velocity(0, 0, 90)
+    time.sleep(1)
+    chassis.set_velocity(0,0,0)   
+
 if __name__ == '__main__':
     print("driving forward")
-    chassis.set_velocity(50, 90, 0)
-    time.sleep(10)
-
-    chassis.set_velocity(0, 0, 0)
-    print("stopping")
+    move_fwd(12)
+    print("Rotating")
+    rotate()
