8b24c7c0f73360687616dd8b67131b7ef020b502
David Foster <davidianfoster@gmail.com>
2024-12-12 07:14:23 +0800

    continued testing rotation and movement

--- Testing.py
+++ Testing.py
@@ -17,19 +17,24 @@ def move_fwd(inches):
     mm = inches * inches_to_mm
     total_drive_time = inches / 12
     current_drive_time = 0
-    while current_drive_time < total_drive_time
+    while current_drive_time < total_drive_time:
         chassis.set_velocity(12*inches_to_mm, 90, 0)
         time.sleep(1)
         current_drive_time += 1
     chassis.set_velocity(0,0,0)
 
 def rotate():
-    chassis.set_velocity(0, 0, 90)
+    chassis.set_velocity(0, 0, 45)
+    time.sleep(1)
+    chassis.set_velocity(0,0,0)   
+    time.sleep(1)
+    chassis.set_velocity(0, 0, -45)
     time.sleep(1)
     chassis.set_velocity(0,0,0)   
 
 if __name__ == '__main__':
     print("driving forward")
     move_fwd(12)
+    time.sleep(2)
     print("Rotating")
     rotate()
